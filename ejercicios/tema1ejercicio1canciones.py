65f9f6b5b580706fc0b2b4df1146f707e869f61e
LuciaAlcarazQA <pqsi2017@gmail.com>
2026-04-20 16:41:59 +0100

    Create tema1ejercicio1canciones.py

---
+++ ejercicios/tema1ejercicio1canciones.py
@@ -0,0 +1,27 @@
+#Obtener valores introducidos por el usuario
+
+Pide a la usuaria que introduzca la siguiente información sobre su canción favorita:
+
+titulo_cancion = input("cual es tu cancion favorita?")
+
+artista = input("quien canta la cancion?")
+
+album = input ("cual es el nombre del album")
+
+ano_lanzamiento = input ("cual es el ano del lanzamiento?")
+
+duracion_en_segundos = input ("cual es la duracion en segundos?")
+
+videoclip = input("¿Tiene videoclip (verdadero o falso)? ")
+
+print("tu cancion favorita es: ", titulo_cancion)
+
+print("la artista quien la canta es: ",artista)
+
+print("pertenece al album: ",album)
+
+print("el ano de lanzamiento es: ",ano_lanzamiento)
+
+print("laduracio de la cancion en segundos es: ",duracion_en_segundos)
+
+print("tiene videoclip?: ", videoclip)
