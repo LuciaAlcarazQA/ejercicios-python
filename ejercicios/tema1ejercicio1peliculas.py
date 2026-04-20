7a73a68023d310aea6b9b16bea75ba949cb0b8e6
LuciaAlcarazQA <pqsi2017@gmail.com>
2026-04-20 16:09:01 +0100

    Ingles

---
+++ ejercicios/tema1ejercicio1peliculas.py
@@ -0,0 +1,41 @@
+#Cadena
+Titulo_de_la_pelicula = "Forrest Gump"
+#Cadena
+Director_de_la_pelicula = "Robert Zemeckis"
+#Numeros (int)
+Ano_de_lanzamiento = 1994
+#Cadena
+Genero = "Drama-comedia"
+#Numeros (int)
+Duracion_en_minutos = 142
+#Booleanos (True o False)
+Tiene_premios = True
+
+
+#Imprimir las variables
+print("Titulo de la pelicula", Titulo_de_la_pelicula)
+print("Director de la pelicula ", Director_de_la_pelicula)
+print("Ano de lanzamiento", Ano_de_lanzamiento)
+print("Genero ", Genero)
+print("Duracion en minutos ", Duracion_en_minutos)
+print("Tiene premios", Tiene_premios)
+
+#Cadena
+movie_title = "Forrest Gump"
+#Cadena
+movie_director = "Robert Zemeckis"
+#Numeros (int)
+release_year = 1994
+#Cadena
+movie_genre = "Comedy-Drama"
+#Numeros (int)
+duration_minutes = 142
+#Booleanos (True o False)
+does_it_have_awards = True
+
+print("Title:", movie_title)
+print("Director:", movie_director)
+print("Year:", release_year)
+print("Genre:", movie_genre)
+print("Duration:", duration_minutes)
+print("Does it have awards?:", does_it_have_awards)
