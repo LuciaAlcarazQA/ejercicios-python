fee80b37d9817d2dc93bba9691fd00af1e950acb
LuciaAlcarazQA <pqsi2017@gmail.com>
2026-04-20 14:27:24 +0100

    tema1ejercicio1

---
+++ ejercicios/tema1ejercicio1.py
@@ -0,0 +1,20 @@
+Titulo_de_la_pelicula = "Forrest Gump"
+
+Director_de_la_pelicula = "Robert Zemeckis"
+
+Ano_de_lanzamiento = 1994
+
+Genero = "Drama-comedia"
+
+Duracion_en_minutos = 142
+
+Tiene_premios = True
+
+
+#Imprimir las variables
+print("Titulo de la pelicula", Titulo_de_la_pelicula)
+print("Director de la pelicula ", Director_de_la_pelicula)
+print("Ano de lanzamiento", Ano_de_lanzamiento)
+print("Genero ", Genero)
+print("Duracion en minutos ", Duracion_en_minutos)
+print("Tiene premios", Tiene_premios)
