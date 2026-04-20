9ac1dfd9a5398251a4e525150894acf97e9b4df6
LuciaAlcarazQA <pqsi2017@gmail.com>
2026-04-20 17:40:18 +0100

    Update tema1ejercicio2canciones.py

--- ejercicios/tema1ejercicio2canciones.py
+++ ejercicios/tema1ejercicio2canciones.py
@@ -1,31 +1,39 @@
 #Obtener valores introducidos por el usuario
 
-#Pide a la usuaria que introduzca la siguiente información sobre su canción favorita:
+# 1 Pide a la usuaria que introduzca la siguiente información sobre su canción favorita:
 
 titulo_cancion = input("cual es tu cancion favorita?")
-
 artista = input("quien canta la cancion?")
-
 album = input ("cual es el nombre del album")
-
 ano_lanzamiento = input ("cual es el ano del lanzamiento?")
-
 duracion_en_segundos = input ("cual es la duracion en segundos?")
-
 videoclip = input("¿Tiene videoclip (verdadero o falso)? ")
 
+#2 Muestra la informacion introducida
 print("tu cancion favorita es: ", titulo_cancion)
-
 print("la artista quien la canta es: ",artista)
-
 print("pertenece al album: ",album)
-
 print("el ano de lanzamiento es: ",ano_lanzamiento)
-
 print("la duracion de la cancion en segundos es: ",duracion_en_segundos)
-
 print("tiene videoclip?: ", videoclip)
 
 #Solicita los mismos datos de la canción que menos le guste.
+
+#3 Solicita los mismos datos de la canción que menos le guste
+titulo_cancion2 = input("cual es tu cancion que menos te gusta? ")
+artista2 = input("quien canta la cancion? ")
+album2 = input("cual es el nombre del album? ")
+ano_lanzamiento2 = input("cual es el ano de lanzamiento? ")
+duracion_en_segundos2 = input("cual es el duracion en segundos? ")
+videoclip2 = input("¿Tiene videoclip (verdadero o falso)? ")
+
+
+#3.1 Muestra la informacion introducida
+print("tu cancion que menos te gusta: ", titulo_cancion2)
+print("la artista que la canta es: ", artista2)
+print("pertenece al album: ", album2)
+print("el ano de lanzaiento es: ", lanzamiento2)
+print("la duracion de la cancion en segundos es de: ", duracion_en_segundos2)
+print("tiene videoclip?: ", videoclip2)
 
 
