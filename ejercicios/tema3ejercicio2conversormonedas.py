5099b560addfce3162d520a74a4bbda0fe9de3a3
LuciaAlcarazQA <pqsi2017@gmail.com>
2026-04-21 16:27:28 +0100

    Update tema3ejercicio2conversormonedas.py

--- ejercicios/tema3ejercicio2conversormonedas.py
+++ ejercicios/tema3ejercicio2conversormonedas.py
@@ -3,7 +3,9 @@ cantidad_euros = float (input("escriba una cantidad en euros: "))
 
 #2. Convierte la cantidad a: dólares (1 euro = 1.1 dólares). Libras (1 euro = 0.87 libras).
 
-dolar
-libras
+dolar = cantidad_euros * 1.1
+libra = cantidad_euros * 0.87
 
-print
+#Muestra los resultados
+print(cantidad_euros, "euros, es igual a", dolar, "dólares.")
+print(cantidad_euros, "euros, es igual a", libra, "libras.")
