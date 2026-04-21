8a77614f25c2cef801a70ce6cb423a019d7caf5b
LuciaAlcarazQA <pqsi2017@gmail.com>
2026-04-21 12:47:44 +0100

    Update tema2ejercicio4correoelectronico.py

--- ejercicios/tema2ejercicio4correoelectronico.py
+++ ejercicios/tema2ejercicio4correoelectronico.py
@@ -3,3 +3,10 @@ correo_electronico = input ("introduce una direccion de correo electronico:")
 
 #2. Muestra: la longitud del correo, el correo en mayúsculas, el correo en minúsculas
 longitud = len (correo_electronico)
+correo_electronicomayusculas = correo_electronico.upper()
+correo_electronicominusculas = correo_electronico.lower()
+
+print(correo_electronico)
+print("la longitud del correo electronico es:", longitud)
+print("el correo electronico en mayusculas es:", correo_electronicomayusculas)
+print("el correo electronico en minusculas es:", correo_electronicominusculas)
