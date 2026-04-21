012a57c17e77609a4f8b46510201fa44fdde9cc0
LuciaAlcarazQA <pqsi2017@gmail.com>
2026-04-21 13:17:12 +0100

    Create tema3ejercicio1preciototal.py

---
+++ ejercicios/tema3ejercicio1preciototal.py
@@ -0,0 +1,12 @@
+#1 Precio total de un producto, Solicita a la usuaria: Nombre del producto, precio por unidad, cantidad a comprar,descuento en porcentaje, IVA en porcentaje.
+nombre_producto = input("escribe el nombre del producto:")
+precio_unidad = float (input("escribe el precio por unidad:"))
+cantidad_a_comprar = int (input("escribe la cantidad a comprar:"))
+descuento_en_porcentaje = float (input("escribe el porcentaje de descuento"))
+iva_en_porcentaje = float (input("escribe el porcentaje de iva"))
+
+#2. Calcula el precio total aplicando descuento e IVA.
+
+
+
+#3.Muestra el resultado final.
