820be5c407bd80fc2eb73e91ec7e8cc0785035a8
LuciaAlcarazQA <pqsi2017@gmail.com>
2026-04-21 12:25:37 +0100

    Update tema2ejercicio3frases.py

--- ejercicios/tema2ejercicio3frases.py
+++ ejercicios/tema2ejercicio3frases.py
@@ -1,1 +1,12 @@
 #1. Pide a la usuaria que introduzca una frase.
+frase = input ("introduce una frase:")
+
+#1.Muestra: la longitud de la frase, la frase en mayúsculas, la frase en minúsculas
+longitud = len (frase)
+frasemayusculas = frase.upper()
+fraseminusculas = frase.lower()
+
+print(frase)
+print("la longitud de la frase es:", longitud)
+print("la frase en mayusculas es:", frasemayusculas)
+print("la frase en minusculas es:", fraseminusculas)
