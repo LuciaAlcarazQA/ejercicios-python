b036684c3568f56a86094cab139c0f5a7847805d
LuciaAlcarazQA <pqsi2017@gmail.com>
2026-04-21 13:23:48 +0100

    Create tema3ejercicio2conversormonedas.py

---
+++ ejercicios/tema3ejercicio2conversormonedas.py
@@ -0,0 +1,5 @@
+#1. Ejercicio 2: Conversor de monedas: pide a la usuaria una cantidad en euros.
+cantidad_euros = float (input("escriba una cantidad en euros: "))
+
+#2. Convierte la cantidad a: dólares (1 euro = 1.1 dólares). Libras (1 euro = 0.87 libras).
+
