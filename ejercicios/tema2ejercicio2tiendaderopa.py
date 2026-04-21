43c724aaeb90c0ff139b051685e0c4dd59f63f29
LuciaAlcarazQA <pqsi2017@gmail.com>
2026-04-21 11:49:47 +0100

    Update tema2ejercicio2tiendaderopa.py

--- ejercicios/tema2ejercicio2tiendaderopa.py
+++ ejercicios/tema2ejercicio2tiendaderopa.py
@@ -1,7 +1,22 @@
 #1. Define los siguientes precios:
 
-Camiseta = 10
-Sudadera = 20.5
-Gorra = 5.5
+camiseta = 10
+sudadera = 20.5
+gorra = 5.5
 
 #2.Pide a la usuaria la cantidad de cada artículo.
+cantidad_camisetas = input("¿cuantas camisetas necesitas comprar?: ")
+cantidad_sudaderas = input("¿cuantas sudaderas necesitas comprar?: ")
+cantidad_gorras = input("¿cuantas gorras necesitas comprar?: ")
+
+#3 Calcula el total de la compra.
+
+total_de_la_compra = (camiseta*cantidad_camisetas) + (sudadera*cantidad_sudaderas) + (gorra*cantidad_gorras)
+
+#4 Añade el 21% de IVA al total
+
+IVA = (total_de_la_compra*21) / 100
+
+#5 Muestra el precio final
+
+precio_final = (total_de_la_compra + IVA)
