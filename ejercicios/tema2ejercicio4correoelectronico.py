65d23005f630e9039911e423b758fb46c48e5df1
LuciaAlcarazQA <pqsi2017@gmail.com>
2026-04-21 12:37:43 +0100

    Create tema2ejercicio4correoelectronico.py

---
+++ ejercicios/tema2ejercicio4correoelectronico.py
@@ -0,0 +1,5 @@
+#1. Pide a la usuaria que introduzca una dirección de correo electrónico.
+correo_electronico = input ("introduce una direccion de correo electronico:")
+
+#2. Muestra: la longitud del correo, el correo en mayúsculas, el correo en minúsculas
+longitud = len (correo_electronico)
