94b90a472ad6358bc6a996bfe1a2f284cceef77c
LuciaAlcarazQA <pqsi2017@gmail.com>
2026-04-28 09:51:31 +0100

    Create tema5ejercicio1calculadoranotamedia.py

---
+++ ejercicios/tema5ejercicio1calculadoranotamedia.py
@@ -0,0 +1,6 @@
+Tema 5. Bucles (2h - 4h)
+Ejercicio 1: Calculadora de nota media
+Pide a la usuaria cuántas notas desea introducir.
+Solicita cada nota.
+Suma todas las notas.
+Calcula y muestra la media.
